119b49a0968013dca5fff5dc215ba30387f00eb6
phuocday <phuocday@gmail.com>
2022-08-20 19:47:30 +0700

    add features

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 let $ = document.querySelector.bind(document)
-let web =  document.documentElement
 let app = {
     result: 0,
     mode: "DectoBin",
@@ -14,21 +13,49 @@ let app = {
     },
     handle: {
         DectoBin: function (value){
-            value = value*1
-            let result = '';
+            let commaSeparation = app.convert.commaSeparation(value)
+            let result = ''
+            value = Math.floor(value)
             while(value>0){
                 result+=value%2
                 value = Math.floor(value/2)
             }
-            return result.split('').reverse().join('')
+            result =  result.split('').reverse().join('')
+            if(commaSeparation.isHaveComma){
+                let comma = ''
+                let i = 0;
+                while(commaSeparation.comma!=1){
+                    i++
+                    comma += Math.floor((commaSeparation.comma*2))
+                    commaSeparation.comma = (commaSeparation.comma*2)%1
+                    if(i==6) break
+                }
+                result += `.${comma}`
+            }
+            return result
         },
         BintoDec: function (value){
+            let commaSeparation = app.convert.commaSeparation(value)
+            value = Math.floor(value*1).toString()
             value = value.split('').reverse()
             let result = 0
             for(let index in value){
                 if(value[index]==1){
                     result+=Math.pow(2,index)
                 }
+            }
+            if(commaSeparation.isHaveComma){
+                commaSeparation.comma = commaSeparation.comma.toString()
+                let comma = 0;
+                let i = 0;
+                let x = commaSeparation.comma
+                for(let i in x ){
+                    if(x[i]==1){
+                        comma+=Math.pow(2,-(i-1))
+                    }
+                }
+                comma = Math.floor(comma*1000000)
+                result += `.${comma}`
             }
             return result
         },
@@ -84,6 +111,21 @@ let app = {
         }
     },
     convert: {
+        commaSeparation: function(value){
+            let result = {
+                comma: 0,
+                isHaveComma: false,
+            }
+            for(let i in value){
+                if(value[i]=='.'){
+                    result.comma = value.split('.')[1]
+                    result.comma = `0.${result.comma}`*1
+                    result.isHaveComma = true
+                    break
+                }
+            }
+            return result
+        },
         letterToNum: function (value){
             let result = []
             for(let index in value){
@@ -221,7 +263,7 @@ let app = {
     checkInput: function (){
         if(select1.value=='Bin'){
             for(let i of input.value){
-                if(i!=1&&i!=0){
+                if(i!=1&&i!=0&&i!='.'){
                     app.result = 'Chỉ được nhập 0 và 1'
                     return false
                 }
@@ -253,3 +295,100 @@ let app = {
     }
 }
 app.handleEvent()
+let control = {
+    element: {
+        menuConvert: $('#menu-convert'),
+        menuTotal: $('#menu-total'),
+        convert: $('#convert'),
+        total: $('#total'),
+
+    },
+    handleEvent: function (){
+        control.element.menuConvert.onclick = function(e){
+            $('.menu-item-active').classList.remove('menu-item-active')
+            e.target.classList.add('menu-item-active')
+            control.element.total.style.display = 'none'
+            control.element.convert.style.display = 'block'
+        }
+        control.element.menuTotal.onclick = function(e){
+            $('.menu-item-active').classList.remove('menu-item-active')
+            e.target.classList.add('menu-item-active')
+            control.element.convert.style.display = 'none'
+            control.element.total.style.display = 'block'
+        }
+        control.element.menuTotal.click()
+    }
+}
+control.handleEvent()
+let app2 = {
+    result: '',
+    mode: 'plus',
+    element: {
+        input1: $('#total-input1'),
+        input2: $('#total-input2'),
+        select: $('#total-select'),
+        again: $('#total-again'),
+        submit: $('#total-submit'),
+        show: $('#total-show'),
+    },
+    handle: {
+        plus: function (value1,value2){
+            value1=app.handle.BintoDec(value1)
+            value2=app.handle.BintoDec(value2)
+            return app.handle.DectoBin(value1+value2)
+        },
+        minus: function (value1,value2){
+            value1=app.handle.BintoDec(value1)
+            value2=app.handle.BintoDec(value2)
+            return app.handle.DectoBin(value1-value2)
+        },
+        multiply: function (value1,value2){
+            value1=app.handle.BintoDec(value1)
+            value2=app.handle.BintoDec(value2)
+            return app.handle.DectoBin(value1*value2)
+        },
+        divide: function (value1,value2){
+            value1=app.handle.BintoDec(value1)
+            value2=app.handle.BintoDec(value2)
+            return app.handle.DectoBin(Math.floor(value1/value2))
+        }
+    },
+    handleEvent: function(){
+        app2.element.select.oninput = function (){
+            app2.mode = app2.element.select.value
+        }
+        app2.element.submit.onclick = function (){
+            if(app2.checkInput()){
+                app2.result = app2.handle[app2.mode](app2.element.input1.value,app2.element.input2.value)
+            }
+            app2.render()
+        }
+        app2.element.again.onclick = function (){
+            app2.element.input1.value = ''
+            app2.element.input2.value = ''
+            app2.element.input1.focus()
+        }
+    },
+    checkInput: function (){
+        for(let i of app2.element.input1.value){
+            if(i!=0&&i!=1){
+                app2.result = 'Chỉ được nhập 0 và 1'
+                return false
+            }
+        }
+        for(let i of app2.element.input2.value){
+            if(i!=0&&i!=1){
+                app2.result = 'Chỉ được nhập 0 và 1'
+                return false
+            }
+        }
+        return true
+    },
+    render: function (){
+        app2.element.show.innerText = app2.result
+    }
+}
+app2.handleEvent()
+
+
+
